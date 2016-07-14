b1b9b1060d5e3a3fd67c66f15f312c0d14392de8
Livre progamacao ltda-me <livre.programacao@gmail.com>
2016-07-14 14:28:28 -0300

    Assunto ( subject line ) Executar o todo-component.
    Descrição ( what happened )
    alterado o app.component.html para chamar o todo-component.
    
    [ticket: X]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { TodoAppComponent } from '../app/todo-app/todo-app.component';
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [TodoAppComponent]
 })
 export class AppComponent {
   title = 'app works! ...and Deployed!';
